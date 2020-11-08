595a63e440509a7447452bc26e82478093170589
supertopoz <deepsleepsounds@gmail.com>
2020-11-09 08:02:23 +0900

    added dice interface

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        self.diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
+        self.diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
+    }
+    
 }
 
