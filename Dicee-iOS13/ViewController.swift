65338ce6a63689a23163347c07ea2f072362f9ce
supertopoz <deepsleepsounds@gmail.com>
2020-11-10 06:02:49 +0900

    completed dice roller

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,17 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        self.diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
-        self.diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
+        
+        let images  = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        self.diceImageView1.image = images[Int.random(in: 0...5)]
+        self.diceImageView2.image = images[Int.random(in: 0...5)]
+    
     }
     
 }
